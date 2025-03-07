c9bbc1693afde9675959ff076371ca0f0296b293
flexafil <flexaf1l@yandex.ru>
2025-03-07 15:17:05 +0300

    ready for 1 review

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,33 @@
 
 // @todo: DOM узлы
 
+const cardContainer = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+function addCard (cardData, deleteCardButton) {
+
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode('true');
+
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', () => {
+    deleteCardButton(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
-
+function deleteCardButton (cardElement) {
+  cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(cardData => {
+  const newCard = addCard(cardData, deleteCardButton);
+  cardContainer.append(newCard);
+});
